392cb682973228b7e637c00a042fb8591aefacde
Yash Kapila <yash.x.kapila@aib.ie>
2021-11-25 09:35:00 +0100

    new modules scaffold

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'list',
+    loadChildren: () => import('./list/list.module').then((m) => m.ListModule),
+  },
+  {
+    path: 'details',
+    loadChildren: () =>
+      import('./details/details.module').then((m) => m.DetailsModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
